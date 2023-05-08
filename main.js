78e6437c88e4ea9b8ca1bab6f23f9f5ea1e8e775
Chri$ Samory <samory.takougne@londo-tech.com>
2023-05-08 12:14:43 -0300

    wip

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+
 menu.onclick=()=>{
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('menu-active');
 }
+
 window.onscroll= ()=>{
     menu.classList.remove('bx-x');
     navbar.classList.remove('menu-active')
 }
 
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-  }
+    document.getElementById("mySidebar").style.width = "40%";
+}
   
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-  }
+function closeNav() {
+ document.getElementById("mySidebar").style.width = "0";
+}
+
+$('.categories-container').on( 'click','div', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+    console.log("filterValue",filterValue);
+  }); 
+
+//   var $grid =  $('.products-container').isotope({
+    // itemSelector: '.box',
+//     percentPosition: true,
+//     transitionDuration: '0.7s',
+//   });
