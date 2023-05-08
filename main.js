055ae1d76d53fbd85de32795d47dfab5ad450e6d
Chri$ Samory <samory.takougne@londo-tech.com>
2023-05-08 07:26:21 -0300

    add sidebar

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ window.onscroll= ()=>{
     menu.classList.remove('bx-x');
     navbar.classList.remove('menu-active')
 }
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
